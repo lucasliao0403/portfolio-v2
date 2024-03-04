b8acce0275e4761700c58b1aa23a8134ec374fe9
Lucas L <lucasliao0403@gmail.com>
2024-03-04 14:09:36 -0500

    added moving banner

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,33 +2,41 @@
 
 import React from 'react';
 import {motion} from 'framer-motion';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useWindowDimensions from '@/utils/useWindowDimensions.'
 
 function Hero(props: any) {
-    let [bannerPosition, setBannerPosition] = useState(0)
+    const [bannerPosition, setBannerPosition] = useState(0)
 
+
+    
 
     return (
-        <div className="flex flex-row bg-blue-purple h-[calc(100vh-300px)]">
-            <div className="flex-1 flex items-center justify-center">
-                <motion.div animate={{
-                // scale: [1, 1, 1, 1, 1],
-                // rotate: [0, 0, 360, 180, 0],
-                // borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-                x:[-500, 0, 500, 1000, 1500, 2000],
+        <div className="flex flex-col bg-blue-purple h-[calc(100vh-300px)] pt-4">
+            <div className="border-solid border-white border-2 border-x-0 py-2">
+                <motion.div 
+                
+                animate={{
+                // TODO: make this change dynamically based on page width
+                x:[-500, 1800],
 
                 }}
                 transition={{
                     duration: 10,
-                    // ease: "easeInOut",
-                    times: [0, 1/6, 2/6, 3/6, 4/6, 5/6],
+                    ease: "linear",
+                    times: [0, 1],
                     repeat: Infinity,
-                    repeatDelay: 0
-                  }}>
+                    repeatDelay: 0.1,
+                }}>
                     <h2 className="text-4xl">
                         test
                     </h2>
                 </motion.div>
+            </div>
+            <div>
+                <div className="flex-1 flex items-center justify-center">
+                    
+                </div>
             </div>
             <div className = "flex-1">
                 
