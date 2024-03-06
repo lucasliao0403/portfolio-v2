6b01ada23641a0f22a243d88052252c60afb74f7
Lucas L <lucasliao0403@gmail.com>
2024-03-06 11:55:20 -0500

    add hover for about me, added experiences title

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Experiences(props: any) {
     return ( // slime scholars, app dev (with photos)
         <div className="bg-orange">
-            wasd
+            <div className="flex justify-center">
+                <div className="text-5xl font-bold text-white py-8 underline decoration-8 decoration-cyan">Experiences</div>
+            </div>
         </div>
     );
 }
