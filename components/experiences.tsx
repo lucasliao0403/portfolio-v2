396b84f9743f5cffe7b0716122226c6f0a83e7ed
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2024-03-10 13:27:16 -0400

    added experience component

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -1,13 +1,42 @@
+'use client'
+
 import React from 'react';
+import { motion } from "framer-motion"
 
 function Experiences(props: any) {
     return ( // slime scholars, app dev (with photos)
         <div className="bg-orange">
-            <div className="flex justify-center">
-                <div className="text-5xl font-bold text-white py-8 underline decoration-8 decoration-cyan">Experiences</div>
+            <div className="flex justify-center flex-col">
+                <div className="text-center text-5xl font-bold text-white py-8 underline decoration-8 decoration-cyan">Experiences</div>
+                <div className=" px-20 pb-8 gap-4 flex flex-col items-center">
+                    <Experience 
+                    company="Slime Scholars" 
+                    title="Developer"
+                    type="Internship"
+                    date="Jan 2024 - Present"
+                    />
+                </div>
             </div>
         </div>
     );
+}
+
+function Experience(props:any) {
+    return (
+        <motion.div 
+            // whileHover = {{scale:1.1,}}
+            className="w-[1000px] bg-white text-black font-bold p-4 cursor-pointer
+            flex flex-col border-solid border-4 border-transparent hover:border-black">
+            <div className="flex justify-between">
+                <h2 className='text-4xl'> {props.company} </h2>
+                <h3 className='text-xl'> {props.type} </h3>
+            </div>
+            <div className="flex justify-between">
+                <h2 className='text-xl'> {props.title} </h2>
+                <h3 className='text-xl'> {props.date} </h3>
+            </div>
+        </motion.div>
+    )
 }
 
 export default Experiences;
