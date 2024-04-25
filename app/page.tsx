9fdf25d59fc94d33f7a07891e094c2899f1c9172
Lucas L. <lucasliao0403@gmail.com>
2024-04-25 11:00:30 -0400

    added icons
    still need to add many more icons

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,21 @@ TODO:
 export default function Home() {
     return (
         <>
+        {/* <head>
+            <link rel="icon" href="/quag.ico" sizes="any" />  
+            <link
+                rel="icon"
+                href="/icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />     
+            <link
+                rel="apple-touch-icon"
+                href="/apple-icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />
+        </head> */}
         <main className="font-mono text-white max-w-[100vw]">
             <Hero/>
             <About/>
--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import React from 'react';
-import { FaRegUser } from "react-icons/fa";
+import { FaRegUser, FaReact } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiTailwindcss } from "react-icons/si";
+
 import Image from 'next/image'
 import {useState} from 'react'
 import { motion, Variants } from "framer-motion"
@@ -15,6 +18,17 @@ const itemVariants: Variants = {
     closed: { opacity: 0, x: 20, transition: { duration: 0.2 } }
   };
 
+
+function Icon({children}:any) {
+    return (
+        <motion.div 
+        initial={{ y: "10vh"}} whileInView={{ x: 0 , y: 0}} transition={{ duration: 1, delay: 0, bounce: 0.5, type: "spring",}} viewport={{ once: true }} whileHover={{scale: 1.0, x:10, y:10, transition: { duration: 0.1, ease:"linear"},}}
+        className=" p-1 bg-yellow drop-shadow-flat text-black border-solid border-black border-4 hover:filter-none text-5xl"
+    >
+        {children}
+        </motion.div>
+    )
+}
 function About(props: any) {
     const[open, setOpen] = useState(false)
 
@@ -32,7 +46,7 @@ function About(props: any) {
                         x:10,
                         y:10,
                         transition: { duration: 0.1, ease:"linear"},}}
-                    className=" p-4 bg-yellow drop-shadow-flat text-black border-solid border-black border-4 hover:filter-none "
+                    className=" p-4 bg-yellow drop-shadow-flat text-black border-solid border-black border-4 hover:filter-none mb-8"
                 >
                     <div className="text-4xl lg:text-5xl flex flex-row gap-4 border-solid border-black border-b-4 mb-2">
                         <FaRegUser className="mt-1 flex justify-items-center items-center"/>
@@ -43,6 +57,11 @@ function About(props: any) {
                         but I'm always eager to work with any exciting technologies that come my way; I'm interested in anything from product design to data analysis. Feel free to explore my page and don't hesitate to contact me - I'd love to talk!
                     </div>
                 </motion.div>
+                <div className="flex flex-row gap-4">
+                   <Icon><FaReact/></Icon>
+                   <Icon><TbBrandNextjs/></Icon>
+                   <Icon><SiTailwindcss/></Icon>
+                </div>
 
             </div>
             <div className="flex-1 flex flex-row justify-between bg-purple border-solid border-white lg:border-l-4 lg:border-t-0 border-t-4 py-32 px-24">
