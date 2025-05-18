5d7abac713b5150db1059dcc36e2eac8886a8120
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2025-05-18 11:22:50 -0400

    added overseer

--- app/data.ts
+++ app/data.ts
@@ -2,12 +2,24 @@ import { Project, Experience } from "@/schema";
 
 export const ProjectList: Project[] = [
     {
+        name: "Overseer",
+        img: "overseer.png",
+        github: "https://github.com/lucasliao0403/overseer",
+        link: "https://devpost.com/software/overseer-vn8fpc",
+        path:"overseer",
+        index: 0,
+        date: "April 2025",
+        pitch: "Mitigating bias in hiring by filtering training data for fairness.",
+        desc: "Overseer mitigates bias in hiring by pruning over-represented groups in hiring datasets.\n\nData is transformed into text embeddings, then clusters are generated to find over-represented groups, which are pruned so no single demographic dominates the training set.\n\nThis balanced dataset can be fed into downstream ML or LLM pipelines, giving recruiters a fairer signal and reducing gender- or ethnicity-based bias in ML-based hiring. Built with a Flask back-end (Cohere API, Scikit-Learn) and a Next.js/Three.js front-end. Oveerseer won “Best DEI AI Hack” at GenAI Genesis 2025.",
+        tags: ["Data Processing", "LLM", "Three.js", "Python", "Flask", "React",],
+    }, 
+    {
         name: "WhaleBeing",
         img: "whalebeing.png",
         github: "https://github.com/lucasliao0403/nameify",
         link: "https://www.whalebeing.co/",
         path:"whalebeing",
-        index: 0,
+        index: 1,
         date: "Jan 2025",
         pitch: "Prediction model for whale-ship collisions.",
         desc: "Won 1st place overall at DeltaHacks 2024. \n\nWhaleBeing uses a dynamic prediction model to display how ship routes interact with blue whale habitats. The core of our model is based off a 2019 UCSD research paper and predicts daily, year-round habitat suitability for blue whales off the coast of California. \n\nBuilt using React, Flask, and Python.",
@@ -22,11 +34,11 @@ export const ProjectList: Project[] = [
         github: "https://github.com/lucasliao0403/nameify",
         link: "https://nameify.vercel.app/",
         path:"nameify",
-        index: 1,
+        index: 2,
         date: "July 2023",
         pitch: "AI Generated Playlist Names",
         desc: "Nameify uses Spotify's Audio Features to analyze track properties such as danceability, energy, and valence. The properties are normalized through a percentile distribution and a sigmoid transform, then passed into GPT-3.5 along with the prompt to generate playlist names. \n\nBuilt using React, NextJS, Node, and Express.",
-        tags: ["Full-Stack Development", "Data Analysis", "OpenAI API", "Node.js", "Express", "Next.js", "React"],
+        tags: ["Full-Stack Development", "LLM","Data Analysis", "OpenAI API", "Node.js", "Express", "Next.js", "React"],
     }, 
     
 ]
@@ -47,20 +59,7 @@ export const ExperienceList: Experience[] = [
 I built wireframes of pages in Figma and implemented designs using our MERN stack, and I worked with our product design group to spearhead our classrooms feature.`,
         tags:["Full-Stack Development", "UI Design", "Product Design", "Next.js API", "Node.js", "React", "Tailwind CSS"]
     }, 
-    // {
-    //     company:"Waterloo Aerial Robotics Group",
-    //     title:"Autonomy Team Developer",
-    //     img:"appDev.png", 
-    //     type:"Extracurricular",
-    //     date:"October 2024 - Present",
-    //     path:"markvilleAppDev",
-    //     index: 2,
-    //     pitch: "",
-    //     github:"",
-    //     link:"",
-    //     desc: `Markville Secondary's App/Web Development club is dedicated to fostering a community of students passionate about App Dev. \n\nWith our team, we planned and taught an exemplary personal portfolio website in HTML, leading 50+ members through its step-by-step creation.`,
-    //     tags:["Front-End Development", "HTML/CSS", "JavaScript", "React", "Teaching"],
-    // }, 
+
     {
         company:"Platform For Youth Creativity",
         title:"Tech Lead",
