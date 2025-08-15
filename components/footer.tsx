b5f93cac9e2666621b4b65ccb0aff90b344f017c
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2025-08-15 15:24:39 -0400

    add se webring

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,6 +7,7 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import {motion} from 'framer-motion'
+import webringLogo from '../app/assets/webring-logo.svg'
 
 function Footer(props:any) {
     return (
@@ -14,7 +15,21 @@ function Footer(props:any) {
             <div className=" flex items-center justify-center font-bold text-sm">
                 <span className="italic">Lucas Liao.</span> 2025
             </div>
-            
+            <Link 
+                href="https://se-webring.xyz/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="flex items-center justify-center hover:opacity-80 transition-opacity duration-200"
+            >
+                <Image 
+                    src={webringLogo} 
+                    alt="SE Webring" 
+                    width={24} 
+                    height={24}
+                    className="filter brightness-0 invert"
+                />
+                <span className="text-sm pl-2">SE Webring</span>
+            </Link>
         </div>
     );
 }
